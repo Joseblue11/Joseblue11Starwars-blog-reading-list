748616c8dfc4f9e9e38ebceccb9a6def7536f883
Jose Antonio Tovar <136499634+Joseblue11@users.noreply.github.com>
2024-05-05 21:07:38 +0000

    se creo el navbar y aun se esta trabajando en el

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,29 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/navbar.css"
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className="navbar navbar-light bg-light mb-3">
+      <div className="container">
+        <Link to="/">
+          <span className="navbar-brand mb-0 h1">
+            <img
+              className="img-navbar"
+              src="https://www.viniloscasa.com/36023-thickbox/vinilos-decorativos-y-pegatinas-logo-star-wars.jpg"
+              alt="logo"
+              style={{ width: "100px", objectFit: "cover" }}
+            />
+          </span>
+        </Link>
+        <div className="ml-auto">
+          <Link to="/demo">
+            <button className="btn btn-primary">
+              Check the Context in action
+            </button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
 };
